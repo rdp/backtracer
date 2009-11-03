69ed16c8ca03363ffdca45dd7637b44f18204411
rdp <rogerpack2005@gmail.com>
2009-11-03 12:42:43 -0700

    work better on doze

--- lib/backtracer.rb
+++ lib/backtracer.rb
@@ -1,21 +1,30 @@
 # this one display full BT with code, at the end [no performance loss]
 
 require File.dirname(__FILE__) + "/shared"
+require 'sane'
 
 at_exit {
- puts "==== "
- if $!
-  backtrace_with_code = $!.backtrace.map{|bt| 
-   file, line, junk = bt.split(":")
-   line = line.to_i - 1 
-   actual_line = Tracer.get_line(file, line)
-   "#{bt}\n\t#{actual_line.strip if actual_line}"
-  } 
-  puts backtrace_with_code
-  puts "===="
- else
-  puts "(no exception to backtrace)"
- end
-   
 
+  if $!
+    puts "==== "
+    bt2 = $!.backtrace
+    backtrace_with_code = $!.backtrace.map{ |bt_line|
+      if OS.windows? && bt_line[1..1] == ':'
+        #["C", "/dev/ruby/allgems/lib/allgems/GemWorker.rb", "91", "in `unpack'"]      
+        drive, file, line, junk = bt_line.split(":")
+        file = drive + ":" + file
+      else
+        file, line, junk = bt_line.split(":")
+      end
+      line = line.to_i
+#      line -= 1 unless line == 0 # not sure if needed
+      actual_line = Tracer.get_line(file, line)
+      "#{bt_line}\n\t#{actual_line.strip if actual_line}"
+    }
+    puts backtrace_with_code
+    puts "===="
+  else
+    puts "(no exception found to backtrace)"
+  end
+  exit!
 }
