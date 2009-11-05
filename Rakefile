3a8ad9178b06019faf36bc3c309f86d4348f4741
rdp <rogerpack2005@gmail.com>
2009-11-05 12:16:01 -0700

    rely on xray

--- Rakefile
+++ Rakefile
@@ -7,7 +7,8 @@ begin
 #    gemspec.homepage = "http://github.com/technicalpickles/the-perfect-gem"
 #    gemspec.description = "TODO"
 #    gemspec.authors = ["Josh Nichols"]
-    gemspec.add_dependency  'sane'
+    gemspec.add_dependency 'sane', '>= 0.9.1'
+    gemspec.add_dependency 'xray'
   end
 rescue LoadError
   puts "Jeweler not available. Install it with: sudo gem install technicalpickles-jeweler -s http://gems.github.com"
--- lib/backtracer_ping.rb
+++ lib/backtracer_ping.rb
@@ -1,4 +1,10 @@
 require 'pp'
+require 'sane/os'
+
+def xray
+ require 'xray'
+ proc { Process.kill "QUIT", Process.pid; '' }
+end
 
 if Thread.current.respond_to? :backtrace
   fella = proc { 
@@ -9,13 +15,23 @@ if Thread.current.respond_to? :backtrace
     out    
   }
 elsif respond_to? :caller_for_all_threads
-  fella = proc {
-    caller_for_all_threads
-  }
+  if OS.windows?
+    fella = proc {
+      caller_for_all_threads
+    }
+  else
+    fella = xray
+  end
 else
  # weak sauce for the old school users :)
- trap("ILL") { puts "All threads:" + Thread.list.inspect, "Current thread:" + Thread.current.to_s, caller } # puts current thread caller
- fella = proc {  Process.kill "ILL", Process.pid } # send myself a signal
+ if OS.windows?
+
+   trap("ILL") { puts "All threads:" + Thread.list.inspect, "Current thread:" + Thread.current.to_s, caller } # puts current thread caller
+   fella = proc {  Process.kill "ILL", Process.pid } # send myself a signal
+ else
+   fella = xray
+ end
+
 end
 
 time = $ping_interval || 5 # seconds
