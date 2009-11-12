d41c6809d1d6a8268b3ebeb9e09fa929a720a17f
rdp <rogerpack2005@gmail.com>
2009-11-12 10:08:02 -0700

    note the screencast

--- lib/backtracer_tracer_exception_creation.rb
+++ lib/backtracer_tracer_exception_creation.rb
@@ -5,13 +5,17 @@ class Demo < EventHook
   def self.process(*args)
 
    # args => [64, #<RuntimeError: hello>, :initialize, Exception]
-   if args[1].is_a? Exception
-     puts args.inspect
+   begin
+     if args[1].is_a? Exception
+       puts args.inspect
+     end
+   rescue 
+     # fantastically, this check fails within rails at times
    end
   end
 end
 
 Demo.start_hook
 
+# TODO 1.9 compat
 #set_trace_func proc {|*args| puts args.inspect }
-raise 'hello'
